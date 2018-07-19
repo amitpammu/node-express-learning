f19f0e6fe9dae29434eee385a130d105dadea548
Amit Kumar <amkumar@Avalon>
2018-07-19 12:01:07 +0530

    changes on hbs file

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res, next) => {
 
     res.render('home', {
-        msg: "Welcome to the express",
+        msg: "Welcome to the express framework for NodeJS",
         pageTitle: "Home",
 
 
